d23e4c6d14f0057c7d06be6f6ecee3929e3d2a05
Joel Udu <udujoel@gmail.com>
2025-05-01 21:57:57 +0100

    Add error handling for post retrieval and update index links to use url_for

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
@@ -16,9 +17,15 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
-@app.route('/post/<int:post_id>')
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
     return render_template('post.html', post=post)
